2dd82664190ae4f58f82675fc39cef0ad30762a6
Ralph Cyrel B. Ronda <justinbebet12@gmail.com>
2024-04-25 13:37:12 +0800

    added pseudocode and the text file

---
+++ file_handling4.py
@@ -0,0 +1,13 @@
+#Ralph Cyrel B. Ronda
+#BSCPE 1-2
+#Creating a text file (double or triple)
+
+#creating a list of 20 integers in text file named "integers.txt"
+
+#create a code that will read the file
+
+
+#creating a code that will find the double and triple of the following integers.
+
+
+#extracting the integers to its respective file
