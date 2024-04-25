6f9b48a47eb41a683538426c30a745f8de45641e
Ralph Cyrel B. Ronda <justinbebet12@gmail.com>
2024-04-25 13:22:32 +0800

    appending the file

--- file_handling3.py
+++ file_handling3.py
@@ -3,8 +3,9 @@
 #Creating a line text (while using loop)
 
 #creating a function to write mylife.txt
-
+def write_to_file(filename):
+    with open(filename. 'a') as file: 
 #will ask the user to enter any line of text and press 'n' to finish the loop
 
-       
+
 #To print the line text into the file
