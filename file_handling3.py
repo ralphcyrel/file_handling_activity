fc3d74e77c176c9716c2ec8c5a8b9be37c08ae21
Ralph Cyrel B. Ronda <justinbebet12@gmail.com>
2024-04-25 13:21:29 +0800

    added pseudocode

---
+++ file_handling3.py
@@ -0,0 +1,10 @@
+#Ralph Cyrel B. Ronda
+#BSCPE 1-2
+#Creating a line text (while using loop)
+
+#creating a function to write mylife.txt
+
+#will ask the user to enter any line of text and press 'n' to finish the loop
+
+       
+#To print the line text into the file
