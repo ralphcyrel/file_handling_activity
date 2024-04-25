b90557db9195bb69be127c75b407321943dfd794
Ralph Cyrel B. Ronda <justinbebet12@gmail.com>
2024-04-25 13:40:18 +0800

    adding the function

--- file_handling4.py
+++ file_handling4.py
@@ -3,7 +3,7 @@
 #Creating a text file (double or triple)
 
 #creating a list of 20 integers in text file named "integers.txt"
-def process_integers(integers,txt, output_double_filename, output_triple_filename):
+def process_integers(integers_txt, output_double_filename, output_triple_filename):
 #create a code that will read the file
 
 
