9b583a7475a2fc27761bb7f8f419c7f01649886c
Ralph Cyrel B. Ronda <justinbebet12@gmail.com>
2024-04-25 13:46:43 +0800

    creating the file of double and triple integers. Also printing the output

--- file_handling4.py
+++ file_handling4.py
@@ -13,4 +13,13 @@ def process_integers(integers_txt, output_double_filename, output_triple_filenam
         even_numbers_squared = [num ** 2 for num in numbers if num % 2 == 0]
         odd_numbers_square = [num ** 3 for num in numbers if num % 2 != 0]
 
+        with open(output_double_filename, "w") as output_double_file:
+            for num_squared in even_numbers_squared:
+                output_double_filewrite(str(num_squared) + "\n")
+
+        with open(output_triple_filename, "w") as output_triple_file:
+            for num_cubed in odd_numbers_cubed:
+                output_triple_file.write(str(num_cubed) + "\n")
+
+        print("Completed. Check the files.")
 #extracting the integers to its respective file
