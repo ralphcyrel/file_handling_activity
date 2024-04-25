4615d4f16aae483b143d4ebb145d40ad1e93e650
Ralph Cyrel B. Ronda <justinbebet12@gmail.com>
2024-04-25 13:11:02 +0800

    adding text file

--- file_handling2.py
+++ file_handling2.py
@@ -28,10 +28,10 @@ def main():
         else:
              print("No valid student data found.")
 
-        except FileNotFoundError:
-            print("Error: File not found.")
-        except Exception as e:
-            print("an error occurred:", e)
+    except FileNotFoundError:
+        print("Error: File not found.")
+    except Exception as e:
+        print("an error occurred:", e)
 
 if __name__ == "__main__":
     main()
