3f95a09c8be6a599e39d25db2cf49c7d24aa71b8
Ralph Cyrel B. Ronda <justinbebet12@gmail.com>
2024-04-25 12:31:07 +0800

    creating a code to find even and odd numbers

--- file_handling1.py
+++ file_handling1.py
@@ -1,16 +1,15 @@
 #Ralph Cyrel B. Ronda
 #BSCPE 1-2
-
+#create a text file name "number.txt" with 20 integers in it.
 #Creating a text file (odd and even)
 def main():
     try:
         #read numbers from "number.txt"
         with open("number.txt", "r") as file:
             numbers = [int(line.strip()) for line in file.readlines()]
-#create a text file name "number.txt" with 20 integers in it.
-
 #extracting even and odd numbers from the file
-
+    even_numbers = [num for num in numbers if num % 2 == 0]
+    odd_numbers = [num for num in numbers if num % 2 != 0]
 #write even number to "even.txt" file.
 
 #write odd number to "odd.txt" file.
