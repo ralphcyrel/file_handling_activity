6fabd75a82c0cd60507b3fc0b94f6a842a7e20a6
Ralph Cyrel B. Ronda <justinbebet12@gmail.com>
2024-04-25 12:29:01 +0800

    reading numbers from the file

--- file_handling1.py
+++ file_handling1.py
@@ -1,7 +1,12 @@
 #Ralph Cyrel B. Ronda
 #BSCPE 1-2
-#Creating a text file (odd and even)
 
+#Creating a text file (odd and even)
+def main():
+    try:
+        #read numbers from "number.txt"
+        with open("number.txt", "r") as file:
+            numbers = [int(line.strip()) for line in file.readlines()]
 #create a text file name "number.txt" with 20 integers in it.
 
 #extracting even and odd numbers from the file
