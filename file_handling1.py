504b775533427edd83945c2206f835b6c32d235f
Ralph Cyrel B. Ronda <justinbebet12@gmail.com>
2024-04-25 12:42:00 +0800

    adding created text

--- file_handling1.py
+++ file_handling1.py
@@ -7,22 +7,28 @@ def main():
         #read numbers from "number.txt"
         with open("number.txt", "r") as file:
             numbers = [int(line.strip()) for line in file.readlines()]
-#extracting even and odd numbers from the file
-    even_numbers = [num for num in numbers if num % 2 == 0]
-    odd_numbers = [num for num in numbers if num % 2 != 0]
-#write even number to "even.txt" file.
-    with open("even.txt", "w") as even file:
-        for num in even_numbers:
-            even_file.write(str(num) + "\n")
-#write odd number to "odd.txt" file.
-    with open("odd.txt", "w") as odd file:
-        for num in odd_numbers:
-            odd_file.write(str(num) + "\n")
-#printing the proof of extracting
-    print("Even and Odd numbers extracted and saved successfully.")
 
-except FileNotFoundError:
-    print("Error: file 'number.txt' not found.")
-except Exception as e:
-    print("An error has occurred: ", e)
+        #extracting even and odd numbers from the file
+        even_numbers = [num for num in numbers if num % 2 == 0]
+        odd_numbers = [num for num in numbers if num % 2 != 0]
 
+        #write even number to "even.txt" file.
+        with open("even.txt", "w") as even_file:
+            for num in even_numbers:
+                even_file.write(str(num) + "\n")
+
+        #write odd number to "odd.txt" file.
+        with open("odd.txt", "w") as odd_file:
+            for num in odd_numbers:
+                odd_file.write(str(num) + "\n")
+
+        #printing the proof of extracting
+            print("Even and Odd numbers extracted and saved successfully.")
+
+    except FileNotFoundError:
+        print("Error: file 'number.txt' not found.")
+    except Exception as e:
+        print("An error has occurred: ", e)
+
+if __name__ == "__main__":
+    main()
