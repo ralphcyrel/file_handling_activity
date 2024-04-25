461b47241edb8d90540ed59837fe8b93c433f011
Ralph Cyrel B. Ronda <justinbebet12@gmail.com>
2024-04-25 12:50:11 +0800

    added pseudocode

---
+++ file_handling2.py
@@ -0,0 +1,12 @@
+#Ralph Cyrel B. Ronda
+#BSCPE 1-2
+#Creating a list of students with GWA
+
+#create a text file named "student_data.txt" with 20 students with GWA in it.
+
+    #Read the student data from file
+
+    #Creating student data and find highest GWA
+
+    #Printing the output
+    
