c4e541e6785640a60888a2e20dcd69783f85e8d9
Ralph Cyrel B. Ronda <justinbebet12@gmail.com>
2024-04-25 12:25:40 +0800

    added pseudocode

---
+++ file_handling1.py
@@ -0,0 +1,13 @@
+#Ralph Cyrel B. Ronda
+#BSCPE 1-2
+#Creating a text file (odd and even)
+
+#create a text file name "number.txt" with 20 integers in it.
+
+#extracting even and odd numbers from the file
+
+#write even number to "even.txt" file.
+
+#write odd number to "odd.txt" file.
+
+#printing the proof of extracting
