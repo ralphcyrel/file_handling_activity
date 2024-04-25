408a485755112e6fc702329c548ad9d7cf95ef1e
Ralph Cyrel B. Ronda <justinbebet12@gmail.com>
2024-04-25 13:43:50 +0800

    doubling and tripling the integers

--- file_handling4.py
+++ file_handling4.py
@@ -8,9 +8,9 @@ def process_integers(integers_txt, output_double_filename, output_triple_filenam
     try:
         with open("integers.txt", "r") as input_file:
             numbers = [int(line.strip()) for line in input_file.readlines()]
-            
 
 #creating a code that will find the double and triple of the following integers.
-
+        even_numbers_squared = [num ** 2 for num in numbers if num % 2 == 0]
+        odd_numbers_square = [num ** 3 for num in numbers if num % 2 != 0]
 
 #extracting the integers to its respective file
