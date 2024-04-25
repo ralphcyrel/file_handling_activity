e10f9ca0283917afa0b6dc03d6321a9a6c419de9
Ralph Cyrel B. Ronda <justinbebet12@gmail.com>
2024-04-25 12:54:50 +0800

    Reading the student data and creating data to fine the students with highest GWA

--- file_handling2.py
+++ file_handling2.py
@@ -3,10 +3,20 @@
 #Creating a list of students with GWA
 
 #create a text file named "student_data.txt" with 20 students with GWA in it.
-
+def main():
+    try:
     #Read the student data from file
-
+        with open("student_data.txt", "r") as file:
+            lines = file.readlines()
     #Creating student data and find highest GWA
-
+        highest_gwa = 0.0
+        highest_gwa_student = ""
+        for line in line:
+            parts = line.strip().split(",")
+        if len(parts) == 2:
+            name, gwa_str = parts
+            try:
+                gwa = float(gwa_str)
+                if gwa > highest_gwa:
+                    highest_gwa = gwa
     #Printing the output
-    
