ee31d0f18126ae377e50bc6af3255f72c6fb7ced
Ralph Cyrel B. Ronda <justinbebet12@gmail.com>
2024-04-25 13:24:20 +0800

    Inputting the line from the user

--- file_handling3.py
+++ file_handling3.py
@@ -4,8 +4,12 @@
 
 #creating a function to write mylife.txt
 def write_to_file(filename):
-    with open(filename. 'a') as file: 
+    with open(filename. 'a') as file:
 #will ask the user to enter any line of text and press 'n' to finish the loop
-
+        while True:
+            line = input("Enter a line to add to the file (or type 'n' to finish)")
+            if line.lower() == 'n':
+                break
+            file.write(line + '\n')
 
 #To print the line text into the file
