f362f1eaf69dde27659fe6392e7f0bcff9bed390
Ralph Cyrel B. Ronda <justinbebet12@gmail.com>
2024-04-25 13:42:16 +0800

    reading the file

--- file_handling4.py
+++ file_handling4.py
@@ -5,7 +5,10 @@
 #creating a list of 20 integers in text file named "integers.txt"
 def process_integers(integers_txt, output_double_filename, output_triple_filename):
 #create a code that will read the file
-
+    try:
+        with open("integers.txt", "r") as input_file:
+            numbers = [int(line.strip()) for line in input_file.readlines()]
+            
 
 #creating a code that will find the double and triple of the following integers.
 
